d246e568356f66d83241eb35e34ac255cbecfcad
mattmaynes <mattmaynes@cmail.carleton.ca>
2014-11-06 14:59:09 -0500

    [Refactoring] Added AutomobileTest

---
+++ Refactoring/src/auto/AutomobileTest.java
@@ -0,0 +1,12 @@
+package auto;
+
+public class AutomobileTest
+{
+	public void race()
+	   {
+	      Automobile car1 = new Automobile();
+	      car1.setMake("Austin Healy");
+	      car1.setModel( "Sprite");
+	      // ...
+	   }
+}
