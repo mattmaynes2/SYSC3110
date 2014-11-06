ef77a51e64cd5d3a87200c48482539313db5ee90
mattmaynes <mattmaynes@cmail.carleton.ca>
2014-11-06 15:03:18 -0500

    [Refactoring] Added MethodSigExample

---
+++ Refactoring/src/sig/MethodCallExample.java
@@ -0,0 +1,12 @@
+package sig;
+
+public class MethodCallExample {
+
+	public void callTest()
+	 {
+		 float r;
+	     MethodSigExample eg = new MethodSigExample();
+	     r = eg.test("hello", 10, "world");
+	 }
+	
+}
---
+++ Refactoring/src/sig/MethodSigExample.java
@@ -0,0 +1,10 @@
+package sig;
+
+public class MethodSigExample
+{
+   public float test(String s, int i, String s2)
+   {
+      int x = i + s.length();
+      return x;
+   }
+}
